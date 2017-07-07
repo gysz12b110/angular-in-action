95d4111d6342bf814404ba08ea92eb84414432c9
gys <gysz12b110@126.com>
2017-07-07 14:51:03 +0800

    first commit

---
+++ angular-in-action/app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+	angular.module('angularAppApp')
+	  .controller('MainCtrl', function ($scope) {
+	    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+	    $scope.addTodo = function () {
+	      $scope.todos.push($scope.todo);
+	      $scope.todo = '';
+	    };
+	    $scope.removeTodo = function (index) {
+	      $scope.todos.splice(index, 1);
+	    };
+	  });	 
